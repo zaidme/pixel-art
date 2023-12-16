5504ee492a470517b7b6c409b53f33f130d0087d
zaid42005 <zaid.42005@gmail.com>
2023-12-16 18:34:02 -0500

    added csv with color palettes (to be expanded later)

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ import pandas as pd
 class Converter():
     def __init__(self) -> None:
         self.color_dict = {}
+    def mosaic(self, img, ratio=0.1):
+        small = cv2.resize(img, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+        return cv2.resize(small, img.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
 
-def mosaic(self, img, ratio=0.1):
-    small = cv2.resize(img, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
-    return cv2.resize(small, img.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+
+    
 
 class Web():
     def __init__(self) -> None:
